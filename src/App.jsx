70ac77b0a865482882ee6f014321b877534897d3
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2025-05-28 11:06:08 +0300

    jsonserver + axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
+import "./App.css";
+import Note from "../components/Note";
+import { useState } from "react";
 
-import './App.css'
+const App = (props) => {
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note');
+  const [showAll, setShowAll] = useState(true);
 
-export default App
+  const handleNoteChange = (e) => {
+    setNewNote(e.target.value);
+  }
+
+  const addNote = (e) => {
+    e.preventDefault();
+    const noteObj = {
+      content: newNote,
+      important: Math.random() > 0.5,
+      id: String(notes.length + 1),
+    }
+
+    setNotes(notes.concat(noteObj));
+    setNewNote('');
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input 
+        value={newNote}
+        onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
+ 
